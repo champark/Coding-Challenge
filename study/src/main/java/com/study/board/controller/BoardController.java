936cf8bc2fe07da6b4aea3bb6cb9cfad1baf04b3
champark <97137143+champark@users.noreply.github.com>
2024-09-28 06:16:24 +0900

    글 작성처리 완료
    JPA를 활용

--- study/src/main/java/com/study/board/controller/BoardController.java
+++ study/src/main/java/com/study/board/controller/BoardController.java
@@ -1,6 +1,8 @@
 package com.study.board.controller;
 
 import com.study.board.entity.Board;
+import com.study.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BoardController {
+
+    @Autowired
+    private BoardService boardService;
 
     @GetMapping("/board/write") // localhost:8090/board/write
     public String boardWriteForm() {
@@ -18,8 +23,7 @@ public class BoardController {
     @PostMapping("/board/writepro")
     public String boardWritePro(Board board) {
 
-        System.out.println("제목 : " + board.getTitle());
-        System.out.println("내용 : " + board.getContent());
+        boardService.write(board);
 
         return "";
     }
