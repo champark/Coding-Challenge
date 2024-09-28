036e0118c367d3a5c7f49974fd5fddf31cbd46c7
champark <97137143+champark@users.noreply.github.com>
2024-09-28 18:53:22 +0900

    thymeleaf를 통한 리스트 구현
    thymeleaf를 이용해 보았다. JSP는 안녕히!

--- study/src/main/java/com/study/board/service/BoardService.java
+++ study/src/main/java/com/study/board/service/BoardService.java
@@ -5,6 +5,8 @@ import com.study.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BoardService {
 
@@ -15,4 +17,8 @@ public class BoardService {
 
         boardRepository.save(board);
     }
+
+     public List<Board> boardList() {
+        return boardRepository.findAll();
+     }
 }
